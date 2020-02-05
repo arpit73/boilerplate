715842663d0f2c99b3437e3750c724ef3b3cb2a2
Arpit Bharti <rpittrer@gmail.com>
2020-02-05 16:36:52 +0530

    finalize

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head'
 const Meta: React.FC = () => (
   <Head>
     <meta charSet="utf-8" />
-    <title>Boilerplate!!!!</title> 
-                        <meta name="theme-color" content="#fff" />
+    <title>Boilerplate!!!!</title>
+    <meta name="theme-color" content="#fff" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="/public/images/favicon.ico" />
   </Head>
